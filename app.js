f5bb55c12c9a9011eee3a82412a7d714e8e5cac1
jb-abhi <jbabhi2@gmail.com>
2022-05-22 12:32:15 +0530

    NEW Commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors");
-
+const port = process.env.PORT || 5000;
 // const nav = [
 //   {
 //     link: "/books",
@@ -58,6 +58,6 @@ app.get("/", function (req, res) {
   res.render("index", {});
 });
 
-app.listen(5000, () => {
+app.listen(port, () => {
   console.log("Server Ready");
 });
